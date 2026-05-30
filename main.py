6b0cae9d1fe54297e99f3573ef479552de302432
mos <pasin.che@ku.th>
2026-05-30 20:19:30 +0700

    ver2

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
+import os
 import firebase_admin
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from firebase_admin import credentials, firestore
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
 
-cred = credentials.Certificate('serviceAccountKey.json')
+cred = credentials.Certificate({
+    "type":                        os.getenv("FIREBASE_TYPE", "service_account"),
+    "project_id":                  os.getenv("FIREBASE_PROJECT_ID"),
+    "private_key_id":              os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key":                 os.getenv("FIREBASE_PRIVATE_KEY", "").replace("\\n", "\n"),
+    "client_email":                os.getenv("FIREBASE_CLIENT_EMAIL"),
+    "client_id":                   os.getenv("FIREBASE_CLIENT_ID"),
+    "auth_uri":                    os.getenv("FIREBASE_AUTH_URI"),
+    "token_uri":                   os.getenv("FIREBASE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_CERT_URL"),
+    "client_x509_cert_url":        os.getenv("FIREBASE_CLIENT_CERT_URL"),
+})
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
